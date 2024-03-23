087bf130d04f61d3d86fd31e8d03505701ab2e9f
Lucas Asevedo <114538415+LucasAsevedoDev@users.noreply.github.com>
2024-03-23 15:13:20 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Head from "../src/components/Head/Head"
+import Content from "./components/Content/Content"
+
+export default function App() {
+  return(
+    <div>
+      <Head />
+      <Content/>
+    </div>
+  )
+}
